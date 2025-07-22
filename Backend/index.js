ed8081c2cc34e4e8aa002490de381d0fcbab9173
YashLagare <yashlagare77@gmail.com>
2025-07-22 17:08:05 +0530

    initial commit

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,17 @@
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import connectDB from './db/connectDB.js';
 import errorHandler from './middleware/errorHandler.js';
 import taskRoutes from './routes/taskRoutes.js';
-
 // Load env vars
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 
 // Connect to database
 connectDB();
@@ -21,19 +26,26 @@ app.use(express.urlencoded({ extended: true }));
 // Routes
 app.use('/api/tasks', taskRoutes);
 
-// Health check route
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Todo API is running!',
-    version: '1.0.0',
-    endpoints: {
-      'GET /api/tasks': 'Get all todos',
-      'POST /api/tasks/create': 'Create new todo',
-      'PUT /api/tasks/update/:id': 'Update todo',
-      'DELETE /api/tasks/delete/:id': 'Delete todo'
-    }
-  });
+const frontendPath = path.join(__dirname, "../dist");
+app.use(express.static(frontendPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(frontendPath, 'index.html'));
 });
+
+// Health check route
+// app.get('/', (req, res) => {
+//   res.json({
+//     message: 'Todo API is running!',
+//     version: '1.0.0',
+//     endpoints: {
+//       'GET /api/tasks': 'Get all todos',
+//       'POST /api/tasks/create': 'Create new todo',
+//       'PUT /api/tasks/update/:id': 'Update todo',
+//       'DELETE /api/tasks/delete/:id': 'Delete todo'
+//     }
+//   });
+// });
 
 // Error handler
 app.use(errorHandler);
