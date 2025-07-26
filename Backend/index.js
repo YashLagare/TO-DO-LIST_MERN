e14966c3630b265cf833c61f9911f0e85e74fe64
YashLagare <yashlagare77@gmail.com>
2025-07-26 17:27:28 +0530

    change in index file

--- Backend/index.js
+++ Backend/index.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(frontendPath));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(frontendPath, dist, "index.html"));
+    res.sendFile(path.join(frontendPath, "index.html"));
   });
 }
 
